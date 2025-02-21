0065a1ec59ea86f1b38e62031dd8be481f7c2d98
praneeth <141117659+Muppasanipraneeth@users.noreply.github.com>
2025-02-21 14:59:00 +0530

    added 3d

---
+++ lumora/app/components/Ani.tsx
@@ -0,0 +1,12 @@
+"use clinet"
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/yUawQzna6OvxfRZH/scene.splinecode" 
+      />
+    </main>
+  );
+}
--- lumora/app/page.tsx
+++ lumora/app/page.tsx
@@ -1,9 +1,11 @@
+import Ani from "./components/Ani";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div className="">
          <Navbar/>
+        <Ani/>
          </div>
   );
 }
